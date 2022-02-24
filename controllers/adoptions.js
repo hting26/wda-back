e8ce4b4215d2fed23418229d59c7e701aa3fea83
hting <26hting@gmail.com>
2022-02-24 11:53:05 +0800

    改submitApply result

--- controllers/adoptions.js
+++ controllers/adoptions.js
@@ -3,40 +3,7 @@ import adoptions from '../models/adoptions.js'
 
 export const submitApply = async (req, res) => {
   try {
-    // if (req.user.cart.length === 0) {
-    //   res.status(400).send({ success: false, message: '購物車是空的' })
-    //   return
-    // }
-    // const hasNotSell = await users.aggregate([
-    //   {
-    //     $match: {
-    //       _id: req.user._id
-    //     }
-    //   },
-    //   {
-    //     $project: {
-    //       'cart.product': 1
-    //     }
-    //   },
-    //   {
-    //     $lookup: {
-    //       from: 'products',
-    //       localField: 'cart.product',
-    //       foreignField: '_id',
-    //       as: 'cart.product'
-    //     }
-    //   },
-    //   {
-    //     $match: {
-    //       'cart.product.sell': false
-    //     }
-    //   }
-    // ])
-    // if (hasNotSell.length > 0) {
-    //   res.status(400).send({ success: false, message: '包含下架商品' })
-    //   return
-    // }
-    const result = await adoptions.create({ user: req.user._id, products: req.user.cart })
+    const result = await adoptions.create({ user: req.user._id }, req.body)
     req.user.cart = []
     await req.user.save()
     res.status(200).send({ success: true, message: '', result: result._id })
